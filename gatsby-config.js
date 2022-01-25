2348650067cd34a4f21ded2f889366851e4eaa51
central.dogma99 <central.dogma99@gmail.com>
2022-01-26 04:04:18 +0900

    Update gatsby-config.js
    change revision id

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         remote: 'origin',
 
         // SHA1 revision to fetch and checkout
-        // revision: '7beed15dd03e03f9f9a183ed3b53f0e51f6dbbd7',
+        revision: '2262c475051c8415f35c3eef805e522d7e3289f3',
 
         // url of the repository to fetch
         url: `https://github.com/centraldogma99/dogma-blog-posts.git`,
