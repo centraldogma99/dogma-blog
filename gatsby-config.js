08218a2b068a134fef69d115dbacb214346e4999
central.dogma99 <central.dogma99@gmail.com>
2022-01-26 22:04:18 +0900

    try 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,7 @@ module.exports = {
     siteUrl: `https://blog.dogmadevs.com`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-emotion`,
-      options: {
-        // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
-        // The values for each key in this example are the defaults the plugin uses.
-        sourceMap: true,
-        autoLabel: 'dev-only',
-        labelFormat: `[local]`,
-        cssPropOptimization: true,
-      },
-    },
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-gatsby-cloud`,
     {
       resolve: 'gatsby-source-filesystem',
