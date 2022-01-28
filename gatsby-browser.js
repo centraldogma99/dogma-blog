940378f58f67d4f741d2152c8658f5722b211f79
central.dogma99 <central.dogma99@gmail.com>
2022-01-29 00:13:10 +0900

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
-/** @jsx jsx */
+import React from 'react';
 import '@fontsource/noto-sans-kr';
-import { jsx } from '@emotion/react';
 import ThemeContextProvider from './src/components/ThemeWrapper';
 
 export const wrapRootElement = ({ element }) => {
